d3c7b1450f09b3e5cf31c1a7237ab33c3e51989f
alexadonov <alexadonovan4427@gmail.com>
2019-08-28 16:23:15 +1000

    changes to home removed

--- src/home.js
+++ src/home.js
@@ -5,7 +5,7 @@ import Menu from "./components/Menu";
 import "./components/styles.css"
 
 
-export default class Shop extends Component {
+export default class Home extends Component {
 
 
   render() {
@@ -19,7 +19,6 @@ export default class Shop extends Component {
             </div>
             <p>Shop now to secure our opening deals</p>
             <a class="btn btn-primary btn-lg" href="sign-in" role="button">Shop now!</a>
-            <a class="btn btn-primary btn-ph" href="purchaseHistory" role="button">Purchase History!</a>
             <div class="row">
               <div class="col">
               <img width="500px" height="312px"class="rounded mx-auto d-block" src="https://o.aolcdn.com/images/dims?resize=2000%2C2000%2Cshrink&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-05%2Fa3672910-8104-11e9-a0f7-aeb448b3c7e2&client=a1acac3e1b3290917d92&signature=b7df459cfd4ed082bc6df7f56cc2daf839ba46fa" alt="My image"/>
