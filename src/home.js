c95db6461928b61a9ab38a9deb507c855cc34f7d
Hector Droguett <hecdroguett@gmail.com>
2019-08-29 20:57:19 +1000

    changes to home

--- src/home.js
+++ src/home.js
@@ -11,8 +11,6 @@ export default class Home extends Component {
     return (
       <div class="content rounded">
         <div className="App">
-          <Menu/>
-          <div class="hidden-thing"/>
           <div class="jumbotron1 rounded">
             <div class="shadow bg-white rounded-top">
             <div class="row px-5 mx-auto">
@@ -57,12 +55,12 @@ export default class Home extends Component {
             </div>
             <div class="row">
               <div class="col thumb-padding item">
-              <img width="500px" height="312px" class="rounded wrapper" src="https://o.aolcdn.com/images/dims?resize=2000%2C2000%2Cshrink&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-05%2Fa3672910-8104-11e9-a0f7-aeb448b3c7e2&client=a1acac3e1b3290917d92&signature=b7df459cfd4ed082bc6df7f56cc2daf839ba46fa" alt="My image"/>
-              <button type="button" class="btn btn-default2 btn-lg"><b>Featured Laptops</b></button>
+              <img width="500px" height="312px" class="rounded wrappera" src="https://o.aolcdn.com/images/dims?resize=2000%2C2000%2Cshrink&image_uri=https%3A%2F%2Fs.yimg.com%2Fos%2Fcreatr-uploaded-images%2F2019-05%2Fa3672910-8104-11e9-a0f7-aeb448b3c7e2&client=a1acac3e1b3290917d92&signature=b7df459cfd4ed082bc6df7f56cc2daf839ba46fa" alt="My image"/>
+              <button type="button" class="btn btn-default2 btn-lg wrapperb"><b>Featured Laptops</b></button>
               </div>
               <div class="col thumb-padding item">
-              <img width="500px" height="312px"class="rounded mx-auto thumb-shadow wrapper" src="https://icdn6.digitaltrends.com/image/microsoft-surface-studio-2-6009-640x640.jpg" alt="My image"/>
-              <button type="button" class="btn btn-default2 btn-lg"><b>Featured Desktops</b></button>
+              <img width="500px" height="312px"class="rounded wrappera" src="https://icdn6.digitaltrends.com/image/microsoft-surface-studio-2-6009-640x640.jpg" alt="My image"/>
+              <button type="button" class="btn btn-default2 btn-lg wrapperb"><b>Featured Desktops</b></button>
               </div>
             </div>
           </div>
