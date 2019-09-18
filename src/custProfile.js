18c87f7db56c044adccf180a60ac40491248b384
jasmine-nahrain <ja5m1ne@hotmail.com>
2019-09-18 16:16:13 +1000

    Added basic security

--- src/custProfile.js
+++ src/custProfile.js
@@ -18,6 +18,9 @@ export default class Shop extends Component {
     }
 
     componentDidMount() {
+      document.getElementById("removeBtn").style.display = "none";
+      document.getElementById("saveBtn").style.display = "none";
+
       axios.get('http://localhost:4000/items/')
           .then(res => {
             this.setState({
@@ -28,14 +31,67 @@ export default class Shop extends Component {
           .catch(function (error) {
               console.log(error);
           });
+
+          if(localStorage.getItem('loggedIn') === 0) {
+            alert('You are not logged in. Please log in to view your details.');
+            window.location = "/sign-in";
+          }
+
     }
 
+    viewPassword() {
+
+      var pass = localStorage.getItem('password');
+      var password = prompt("Please enter your password");
+      if(password === pass) {
+        document.getElementById("showBtn").style.display = "none";
+        document.getElementById('pass').innerHTML = pass;
+        document.getElementById("removeBtn").style.display = "inline";
+      } else {
+        alert("Sorry, wrong password");
+      }
+    }
+
+    hidePassword() {
+
+      document.getElementById("showBtn").style.display = "inline";
+      document.getElementById('pass').innerHTML = "********";
+      document.getElementById("removeBtn").style.display = "none";
+    }
+
+    editName() {
+      var name = localStorage.getItem('name');
+      var pass = localStorage.getItem('password');
+
+      var password = prompt("Please enter your password");
+      if(password === pass) {
+        var newName = prompt("Please enter your new name");
+        localStorage.setItem('name', newName);
+      } else {
+        alert("Sorry, wrong password");
+      }
+    }
+
+    editEmail() {
+      document.getElementById('emailInput').disabled = false;
+      document.getElementById("showEmailBtn").style.display = "none";
+      document.getElementById("saveBtn").style.display = "inline";
+    }
+
+    saveBtn() {
+      //Update database here
+      document.getElementById('emailInput').disabled = true;
+      document.getElementById("showEmailBtn").style.display = "inline";
+      document.getElementById("saveBtn").style.display = "none";
+      var id = this.props.match.params.id;
+      axios.post('http://localhost:4000/items/update/' + id, this.state.email)
+        .then(res => console.log(res.data));
+        }
 
   render() {
     return (
     <div class="container">
         <Router>
-        <Menu/>
             <div className="App">
                 <div class="jumbotron">
                     <div class="shadow p-3 mb-5 bg-white rounded">
@@ -46,30 +102,102 @@ export default class Shop extends Component {
                         <h2>Personal Details</h2>
                     </div>
                     <div>
-                        <h3>Name: </h3>
-                        <h5>{localStorage.getItem('username')}</h5>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <h3>Name: </h3>
+                        </div>
+                        <div class="col-sm">
+                          <h5>{localStorage.getItem('username')}</h5>
+                        </div>
+                        <div class="col-sm">
+                          <button onClick={this.viewPassword} id="editName" class="btn btn-outline-primary">Edit Name</button>
+                        </div>
+                    </div></div>
                     </div>
                     <div>
-                        <h3>Date of Birth: </h3>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <h3>Date of Birth: </h3>
+                        </div>
+                        <div class="col-sm">
+                          <h5>{localStorage.getItem('dob')}</h5>
+                        </div>
+                        <div class="col-sm">
+                        <button onClick={this.viewPassword} id="editDOB" class="btn btn-outline-primary">Edit Date of Birth</button>
+                        </div>
+                    </div></div>
                     </div>
                     <div>
-                        <h3>Password: </h3>
-                        <h5>{localStorage.getItem('password')}</h5>
-                    </div>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <h3>Password: </h3>
+                        </div>
+                        <div class="col-sm">
+                          <h5 id="pass">********</h5>
+                        </div>
+                        <div class="col-sm">
+                          <button onClick={this.viewPassword} id="showBtn" class="btn btn-outline-primary">View Password</button>
+                          <button onClick={this.hidePassword} id="removeBtn" class="btn btn-outline-primary">Hide Password</button>
+                          <button onClick={this.editPassword} id="showBtn" class="btn btn-outline-primary">Edit Password</button>
 
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-sm">
+                          <h3>User Type </h3>
+                        </div>
+                        <div class="col-sm">
+                          <h5>{localStorage.getItem('userType')}</h5>
+                        </div>
+                        <div class="col-sm"></div>
+                    </div></div>
                     <br></br>
                     <br></br>
 
                     <div>
                         <h2>Contact Details</h2>
                     </div>
+
                     <div>
-                        <h3>Email: </h3>
-                    </div>
-                    <div>
-                        <h3>Phone: </h3>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-sm">
+                            <h3>Email: </h3>
+                          </div>
+                          <div class="col-sm">
+                            <input type="text" placeholder={localStorage.getItem('email')} class="form-control" id="emailInput" disabled></input>
+                          </div>
+                          <div class="col-sm">
+                          <button onClick={this.editEmail} id="showEmailBtn" class="btn btn-outline-primary">Edit Email</button>
+                          <button onClick={this.saveBtn} id="saveBtn" class="btn btn-outline-primary">Save Email</button>
+                          </div>
+                        </div>
+                      </div>
                     </div>
 
+                    <div>
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-sm">
+                            <h3>Phone: </h3>
+                          </div>
+                          <div class="col-sm">
+                            <h5>{localStorage.getItem('phone')}</h5>
+                          </div>
+                          <div class="col-sm">
+                          <button onClick={this.editPhone} id="showBtn" class="btn btn-outline-primary">Edit Phone Number</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <br/><br/>
                     <a class="btn btn-primary btn-lg" href="/edit-user" role="button">Edit Profile</a>
                 </div>
             </div>
--- src/sign-in.js
+++ src/sign-in.js
@@ -16,12 +16,19 @@ export default class Shop extends Component {
        this.onChangePassword = this.onChangePassword.bind(this);
        this.onSubmit = this.onSubmit.bind(this);
 
+       //Tells the front end we are calling these fields from the database
        this.state = {
            username: String,
-           password: String
+           password: String,
+           email: String,
+          phone: String,
+          dob: String,
+          loggedIn: Boolean
        }
     }
 
+    //When the value changes, saves it locally
+    //This also needs to be called in the input field so it can be recognised
     onChangeUsername(e) {
       this.setState({
           username: e.target.value
@@ -36,22 +43,30 @@ export default class Shop extends Component {
     onSubmit(e) {
           e.preventDefault();
 
+          //So the program knows to acknowledge the locally saved data
           const newUser = {
             username: this.state.username,
             password: this.state.password
         };
 
-          axios.get('http://localhost:4000/users/')
+          axios.get('http://localhost:4000/users/') //Calls the webpage that saves all the data
               .then(response => {
-                for(var i = 0; i < response.data.length; i++) {
-                  if(response.data[i].username === newUser.username && response.data[i].password === newUser.password  ) {
+                for(var i = 0; i < response.data.length; i++) { //Going through the data
+                  if(response.data[i].username === newUser.username && response.data[i].password === newUser.password  ) { //finding a match
                       localStorage.setItem('username', newUser.username);
-                      localStorage.setItem('password', newUser.password);
-alert("Yes");
-                      // this.props.history.push('/custProfile')
+                      localStorage.setItem('password', newUser.password); //Saves the data found to a local drive so you can call in other functions
+                      localStorage.setItem('email', response.data[i].email);
+                      localStorage.setItem('phone', response.data[i].phone);
+                      localStorage.setItem('userType', response.data[i].userType);
+                      localStorage.setItem('dob', response.data[i].dob);
+                      localStorage.setItem('loggedIn', 1);
+
+                      window.location = "/custProfile";
+
                       return;
                     }
                 }
+                //Just a basic check that it doesnt match
                 if(!(response.data.username === newUser.username) || !(response.data.password === newUser.password)) {
                   alert("Wrong username and/or password");
                   return;
@@ -61,6 +76,7 @@ alert("Yes");
                   console.log('What happened? ' + error);
               })
 
+              //resets the fields to blank after everything has been done.
             this.setState({
               username: '',
               password : '',
