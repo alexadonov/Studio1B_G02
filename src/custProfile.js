f8d0a142d784ca72ff0972be91be67f6e481e82e
alexadonov <alexadonovan4427@gmail.com>
2019-09-18 17:17:46 +1000

    removed custProfile changes

--- src/custProfile.js
+++ src/custProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./components/Menu";
-import "./components/styles.css"
+import "./static/styles.css"
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from 'axios';
 
@@ -18,9 +18,6 @@ export default class Shop extends Component {
     }
 
     componentDidMount() {
-      document.getElementById("removeBtn").style.display = "none";
-      document.getElementById("saveBtn").style.display = "none";
-
       axios.get('http://localhost:4000/items/')
           .then(res => {
             this.setState({
@@ -31,62 +28,8 @@ export default class Shop extends Component {
           .catch(function (error) {
               console.log(error);
           });
-
-          if(localStorage.getItem('loggedIn') === 0) {
-            alert('You are not logged in. Please log in to view your details.');
-            window.location = "/sign-in";
-          }
-
     }
 
-    viewPassword() {
-
-      var pass = localStorage.getItem('password');
-      var password = prompt("Please enter your password");
-      if(password === pass) {
-        document.getElementById("showBtn").style.display = "none";
-        document.getElementById('pass').innerHTML = pass;
-        document.getElementById("removeBtn").style.display = "inline";
-      } else {
-        alert("Sorry, wrong password");
-      }
-    }
-
-    hidePassword() {
-
-      document.getElementById("showBtn").style.display = "inline";
-      document.getElementById('pass').innerHTML = "********";
-      document.getElementById("removeBtn").style.display = "none";
-    }
-
-    editName() {
-      var name = localStorage.getItem('name');
-      var pass = localStorage.getItem('password');
-
-      var password = prompt("Please enter your password");
-      if(password === pass) {
-        var newName = prompt("Please enter your new name");
-        localStorage.setItem('name', newName);
-      } else {
-        alert("Sorry, wrong password");
-      }
-    }
-
-    editEmail() {
-      document.getElementById('emailInput').disabled = false;
-      document.getElementById("showEmailBtn").style.display = "none";
-      document.getElementById("saveBtn").style.display = "inline";
-    }
-
-    saveBtn() {
-      //Update database here
-      document.getElementById('emailInput').disabled = true;
-      document.getElementById("showEmailBtn").style.display = "inline";
-      document.getElementById("saveBtn").style.display = "none";
-      var id = this.props.match.params.id;
-      axios.post('http://localhost:4000/items/update/' + id, this.state.email)
-        .then(res => console.log(res.data));
-        }
 
   render() {
     return (
@@ -99,105 +42,32 @@ export default class Shop extends Component {
                     </div>
 
                     <div>
-                        <h2>Personal Details</h2>
+                        <h3>Personal Details</h3>
                     </div>
-                    <div>
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-sm">
-                          <h3>Name: </h3>
-                        </div>
-                        <div class="col-sm">
-                          <h5>{localStorage.getItem('username')}</h5>
-                        </div>
-                        <div class="col-sm">
-                          <button onClick={this.viewPassword} id="editName" class="btn btn-outline-primary">Edit Name</button>
-                        </div>
-                    </div></div>
-                    </div>
-                    <div>
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-sm">
-                          <h3>Date of Birth: </h3>
-                        </div>
-                        <div class="col-sm">
-                          <h5>{localStorage.getItem('dob')}</h5>
-                        </div>
-                        <div class="col-sm">
-                        <button onClick={this.viewPassword} id="editDOB" class="btn btn-outline-primary">Edit Date of Birth</button>
-                        </div>
-                    </div></div>
-                    </div>
-                    <div>
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-sm">
-                          <h3>Password: </h3>
-                        </div>
-                        <div class="col-sm">
-                          <h5 id="pass">********</h5>
-                        </div>
-                        <div class="col-sm">
-                          <button onClick={this.viewPassword} id="showBtn" class="btn btn-outline-primary">View Password</button>
-                          <button onClick={this.hidePassword} id="removeBtn" class="btn btn-outline-primary">Hide Password</button>
-                          <button onClick={this.editPassword} id="showBtn" class="btn btn-outline-primary">Edit Password</button>
+                    <p>
+                        <strong>Name: </strong> {localStorage.getItem('username')}
+                    </p>
+                    <p>
+                        <strong>Date of Birth: </strong> {localStorage.getItem('dob')}
+                    </p>
+                    <p>
+                        <strong>Password: </strong> {localStorage.getItem('password')}
+                    </p>
 
-                        </div>
-                    </div>
-                    </div>
-                    </div>
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-sm">
-                          <h3>User Type </h3>
-                        </div>
-                        <div class="col-sm">
-                          <h5>{localStorage.getItem('userType')}</h5>
-                        </div>
-                        <div class="col-sm"></div>
-                    </div></div>
                     <br></br>
                     <br></br>
 
                     <div>
-                        <h2>Contact Details</h2>
+                        <h3>Contact Details</h3>
                     </div>
+                    <p>
+                        <strong>Email: </strong> {localStorage.getItem('email')}
+                    </p>
+                    <p>
+                        <strong>Phone: </strong> {localStorage.getItem('phone')}
+                    </p>
 
-                    <div>
-                      <div class="container">
-                        <div class="row">
-                          <div class="col-sm">
-                            <h3>Email: </h3>
-                          </div>
-                          <div class="col-sm">
-                            <input type="text" placeholder={localStorage.getItem('email')} class="form-control" id="emailInput" disabled></input>
-                          </div>
-                          <div class="col-sm">
-                          <button onClick={this.editEmail} id="showEmailBtn" class="btn btn-outline-primary">Edit Email</button>
-                          <button onClick={this.saveBtn} id="saveBtn" class="btn btn-outline-primary">Save Email</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div>
-                      <div class="container">
-                        <div class="row">
-                          <div class="col-sm">
-                            <h3>Phone: </h3>
-                          </div>
-                          <div class="col-sm">
-                            <h5>{localStorage.getItem('phone')}</h5>
-                          </div>
-                          <div class="col-sm">
-                          <button onClick={this.editPhone} id="showBtn" class="btn btn-outline-primary">Edit Phone Number</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <br/><br/>
+                    <br></br>
                     <a class="btn btn-primary btn-lg" href="/edit-user" role="button">Edit Profile</a>
                 </div>
             </div>
