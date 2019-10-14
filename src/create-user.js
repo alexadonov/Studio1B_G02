b143bda81a2f5438bbd5561fe19d5adc7e5b0a79
alexadonov <alexadonovan4427@gmail.com>
2019-10-14 16:56:00 +1100

    redo create-user

--- src/create-user.js
+++ src/create-user.js
@@ -14,6 +14,9 @@ export default class Shop extends Component {
      this.onChangeUsername = this.onChangeUsername.bind(this);
      this.onChangePassword = this.onChangePassword.bind(this);
      this.onChangeDOB = this.onChangeDOB.bind(this);
+     this.onChangeDate = this.onChangeDate.bind(this);
+     this.onChangeMonth = this.onChangeMonth.bind(this);
+     this.onChangeYear = this.onChangeYear.bind(this);
      this.onChangeEmail = this.onChangeEmail.bind(this);
      this.onChangePhone = this.onChangePhone.bind(this);
      this.onChangeUserType = this.onChangeUserType.bind(this);
@@ -64,6 +67,24 @@ export default class Shop extends Component {
    });
   }
 
+  onChangeDate(e) {
+    this.setState({
+        date: e.target.value
+    });
+  }
+
+  onChangeMonth(e) {
+   this.setState({
+       month: e.target.value
+   });
+  }
+
+  onChangeYear(e) {
+   this.setState({
+       year: e.target.value
+   });
+  }
+
   onChangeUserType(e) {
    this.setState({
        userType: e.target.value
@@ -72,12 +93,18 @@ export default class Shop extends Component {
 
   onSubmit(e) {
         e.preventDefault();
+        var date = this.state.date;
+        var month = this.state.month;
+        var year = this.state.year;
+
+        var DOB = this.state.date + "/" + this.state.month + "/" + this.state.year;
+        localStorage.setItem('dob', DOB);
 
         const newUser = {
           username: this.state.username,
           password: this.state.password,
           email: this.state.email,
-          dob: this.state.dob,
+          dob: DOB,
           phone: this.state.phone,
           userType: this.state.userType
       };
@@ -88,12 +115,26 @@ export default class Shop extends Component {
               localStorage.setItem('password', newUser.password);
               localStorage.setItem('email', newUser.email);
               localStorage.setItem('phone', newUser.phone);
+              localStorage.setItem('dob', newUser.dob);
               localStorage.setItem('userType', newUser.userType);
                 window.location = "/";
             })
             .catch(function (error){
                 console.log('What happened? ' + error);
             })
+
+            axios.get('http://localhost:4000/users/') //Calls the webpage that saves all the data
+            .then(response => {
+              for(var i = 0; i < response.data.length; i++) { //Going through the data
+                if((response.data[i].username === localStorage.getItem('username')) && (response.dara[i].password === localStorage.getItem('password'))) {
+                  localStorage.setItem('currentUserId', response.data[i]._id);
+                  console.log(response.data[i]._id);
+                }
+                          }
+                        })
+                        .catch(function (error){
+                            console.log('What happened? ' + error);
+                        })
 
           this.setState({
             username: '',
@@ -110,97 +151,83 @@ export default class Shop extends Component {
         <Router>
           <Menu/>
           <div className="App" >
-            <br/>
-            <div class="jumbotron">
-              <h1>Create User</h1>
-              <form onSubmit={this.onSubmit}>
+          <br/>
+          <div class="jumbotron">
+            <h1>Create User</h1>
+            <form onSubmit={this.onSubmit}>
 
-                <div class="form-group">
-                  <input type="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={this.state.username} onChange={this.onChangeUsername} required/>
-                </div>
-
-                <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" value={this.state.password} onChange={this.onChangePassword} required/>
-                </div>
-
-                <div class="form-group">
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" value={this.state.email} onChange={this.onChangeEmail}/>
-                </div>
-
-                <div class="form-group">
-                  <input type="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Phone Number" value={this.state.phone} onChange={this.onChangePhone}/>
-                </div>
-
-                <div class="form group">
-                  <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Year (yyyy)" id="year" value={this.state.dob} onChange={this.onChangeDOB}/>
-                </div>
-
-                <div class="col-lg-8 col-lg-offset-8">
-                  <div class="form-group sm-3">
-                    <div><h5>Account Type?:</h5></div>
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <div class="row">
-
-                    <div class="col-sm">
-                      <select id="inputState" class="form-control" id="date" value={this.state.date} onChange={this.onChangeDate}>
-                        <option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option><option>44</option><option>12</option>
-                        <option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option><option>21</option><option>22</option><option>23</option><option>24</option>
-                        <option>25</option><option>26</option><option>27</option><option>28</option><option>29</option><option>30</option><option>31</option>
-                      </select>
-                    </div>
-
-                    <div class="col-sm">
-                      <select id="inputState" class="form-control" value={this.state.month} onChange={this.onChangeMonth} id="month">
-                        <option selected>January</option><option>February</option><option>March</option><option>April</option><option>May</option><option>June</option>
-                        <option>July</option><option>August</option><option>September</option><option>October</option><option>November</option><option>December</option>
-                      </select>
-                    </div>
-
-                    <div class="col-sm">
-                      <input type="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Year (yyyy)" id="year" value={this.state.year} onChange={this.onChangeYear}/>
-                    </div>
-
-                  </div>
-                </div>
-
-                <div class="container">
-                  <div class="row">
-                    <div class="col">
-                      <h5>Who are you?</h5>
-                    </div>
-
-                    <div class="col">
-                      <div class="custom-control custom-radio">
-                        <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
-                        <label class="custom-control-label" for="customControlValidation2">User</label>
-                      </div>
-                    </div>
-
-                    <div class="col">
-                      <div class="custom-control custom-radio mb-3">
-                      <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
-                      <label class="custom-control-label" for="customControlValidation3">Reatiler</label>
-                      <div class="invalid-feedback">Please check a box</div>
-                    </div>
-
-                    <div class="col">
-                      <div class="custom-control custom-radio mb-3">
-                        <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
-                        <label class="custom-control-label" for="customControlValidation3">Admin</label>
-                        <div class="invalid-feedback">Please check a box</div>
-                      </div>
-                    </div>
-
-                  </div>
-                </div>
-                </div>
-                <input type="submit" class="btn btn-outline-primary" value="Submit"/>
-              </form>
+            <div class="form-group">
+              <input type="username" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value={this.state.username} onChange={this.onChangeUsername} required/>
             </div>
+
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" value={this.state.password} onChange={this.onChangePassword} required/>
+            </div>
+
+            <div class="form-group">
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" value={this.state.email} onChange={this.onChangeEmail}/>
+            </div>
+
+
+            <div class="form-group">
+              <input type="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Phone Number" value={this.state.phone} onChange={this.onChangePhone}/>
+            </div>
+
+            <div class="form-group">
+              <div class="row">
+
+                <div class="col-sm">
+                  <input type="username" class="form-control" aria-label="Date" placeholder="Date (dd)" value={this.state.date} onChange={this.onChangeDate}/>
+                </div>
+
+                <div class="col-sm">
+                  <input id="inputState" class="form-control" placeholder="Month (mm)" value={this.state.month} onChange={this.onChangeMonth} id="month"/>
+                </div>
+
+                <div class="col-sm">
+                  <input type="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Year (yyyy)" id="year" value={this.state.year} onChange={this.onChangeYear}/>
+                </div>
+
+                </div>
+            </div>
+
+            <div class="container">
+              <div class="row">
+              <div class="col">
+                  <h5>Who are you?</h5>
+              </div>
+
+                <div class="col">
+                  <div class="custom-control custom-radio">
+                    <input type="radio" class="custom-control-input" id="customControlValidation2" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
+                    <label class="custom-control-label" for="customControlValidation2">User</label>
+                  </div>
+                </div>
+
+              <div class="col">
+                <div class="custom-control custom-radio mb-3">
+                  <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
+                  <label class="custom-control-label" for="customControlValidation3">Reatiler</label>
+                  <div class="invalid-feedback">Please check a box</div>
+                </div>
+              </div>
+
+              <div class="col">
+                <div class="custom-control custom-radio mb-3">
+                  <input type="radio" class="custom-control-input" id="customControlValidation3" name="radio-stacked" value={this.state.userType} onChange={this.onChangeUserType} required/>
+                  <label class="custom-control-label" for="customControlValidation3">Admin</label>
+                  <div class="invalid-feedback">Please check a box</div>
+                </div>
+              </div>
+
           </div>
+          </div>
+          <input type="submit" class="btn btn-outline-primary" value="Submit"/>
+          </form>
+        </div>
+        </div>
+
+
         </Router>
       </div>
     );
