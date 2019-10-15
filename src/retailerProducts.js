c077794676e3ba455d7c31b7b61d899c67bd330d
alexadonov <alexadonovan4427@gmail.com>
2019-10-15 11:44:05 +1100

    product page done - just needs edit

--- src/retailerProducts.js
+++ src/retailerProducts.js
@@ -13,92 +13,99 @@ export default class Shop extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { items: []
-            // name: String,
-            // price: String,
-            // description: String,
-            // brand: String,
-            // model: String,
-            // inStock: Boolean,
-            // image: String
+ 
+        this.state = {
+          items: [],
+          users: [],
         }
-    }
-
-    componentDidMount() {
-        axios.get('http://localhost:4000/items/')
-            .then(res => {
-                this.setState({
-                    items: res.data
-                })
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    addToCart(currentItem) {
-        localStorage.setItem('name', currentItem.name);
-    }
-
-
-    render() {
-
-        return (
-
-            <div class="content rounded">
-                <head><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/></head>
-                <div className="App">
-                    <Router>
-                    </Router>
-                    <div class="jumbotron1">
-                        <Menu/>
-                        <div class="row">
-                            <div class="mainContent pb-4">
-                                <section class="pt-0 pb-5">
-                                    <div class="row pb-5">
-                                        <div class="card-deck pb-3 px-4">
-                                            {this.state.items.map(function(currentItem, i) {
-                                                return (
-                                                    <div class="row">
-                                                        <div class="card-deck py-3 px-4">
-                                                            <div class="card" key={i}>
-                                                                <img src={currentItem.image} class="card-img-top card-header" alt="placeholder"/>
-                                                                <div class="card-body">
-                                                                    <h5 class="card-title"><b>{currentItem.name}</b></h5>
-                                                                    <p class="card-text">{currentItem.description}</p>
-                                                                </div>
-                                                                <div class="card-footer">
-                                                                    <a class="price my-2"> <b><CurrencyFormat value={currentItem.price} displayType="text" thousandSeparator={true} prefix="$" /></b></a>
-                                                                    <button class="list-group-item list-group-item-action" onClick={localStorage.setItem('name', currentItem)}>Edit Product</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                )
-                                            })
-                                            }
-                                        </div>
-                                    </div>
-                                </section>
-                            </div>
-                            <div class = "sideBar col-3 px-3 py-3">
-                                <aside>
-                                    <h2>FILTERS</h2>
-                                    <div class="list-group list-group-flush">
-                                        <button type="button" class="list-group-item list-group-item-action">Keyboard</button>
-                                        <button type="button" class="list-group-item list-group-item-action">Mouse</button>
-                                        <button type="button" class="list-group-item list-group-item-action">Webcam</button>
-                                        <button type="button" class="list-group-item list-group-item-action">Fan</button>
-                                        <button type="button" class="list-group-item list-group-item-action">Speaker</button>
-                                        <button type="button" class="list-group-item list-group-item-action">Cleaner</button>
-                                    </div>
-                                </aside>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+ 
+     }
+ 
+     componentDidMount() {
+ 
+       // if(localStorage.getItem('userType') != 'admin' || localStorage.getItem('userType') != '0') {
+       //   alert("You are not admin and cannot access this page")
+       //   window.location = "/";
+       // }
+ 
+       axios.get('http://localhost:4000/items/')
+           .then(res => {
+             this.setState({
+               items: res.data
+             })
+           })
+           .catch(function (error) {
+               console.log(error);
+           });
+           axios.get('http://localhost:4000/users/')
+               .then(res => {
+                 this.setState({
+                   users: res.data
+                 })
+               })
+               .catch(function (error) {
+                   console.log(error);
+               });
+     }
+ 
+   render() {
+     return (
+       <div class="container" >
+           <Router>
+           <div className="App">
+           <br/>
+           <div class="shadow p-1 mb-5 bg-white rounded-top">
+             <h1>Retailer Products</h1>
+             <div class="row">
+             <div class="col-sm"></div>
+             <div class="col-sm">
+                <a href="/custProfile">Return to Profile</a>
+             </div>
+             <div class="col-sm"></div>
+               <div class="col-sm">
+                 <a class="btn btn-link" href="/admin-create-item">Add new item</a>
+               </div>
+               <div class="col-sm"></div><div class="col-sm"></div>
+             </div>
+ 
+             <table className="table table-striped" style={{ marginTop: 20 }} >
+               <thead>
+                 <tr>
+                   <th>Item ID</th>
+                   <th>Name</th>
+                   <th>Price</th>
+                   <th>Brand</th>
+                   <th>Edit</th>
+                   <th>Delete</th>
+                 </tr>
+               </thead>
+             <tbody>
+             {this.state.items.map(function(currentItem, i) {
+                 if(currentItem.retailerId == localStorage.getItem('currentUserId')){
+                   return (
+                     <tr>
+                       <td>{currentItem._id}</td>
+                         <td>{currentItem.name}</td>
+                         <td>${currentItem.price}</td>
+                         <td>{currentItem.brand}</td>
+                         <td>{localStorage.setItem('editId', currentItem._id)}<button class="btn btn-link"><a href={"/"}>Edit</a></button></td>
+                         <td><button class="btn btn-link" onClick={function() {
+                           localStorage.setItem('deleteId', currentItem._id);
+                           axios.delete('http://localhost:4000/items/' + localStorage.getItem('deleteId'))
+                             .then(res => alert("Deleted"), window.location = '/');
+                         }}>X</button></td>
+                     </tr>
+                   )
+                        }
+                 })
+               }
+             </tbody>
+             </table>
+ 
+           </div>
+           </div>
+         </Router>
+       </div>
+     );
+   }
 }
