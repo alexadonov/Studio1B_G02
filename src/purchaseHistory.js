7246f801839b74937a9f260abb78c24ecb3dc23d
Calvin Dong <calvin.dong156@gmail.com>
2019-10-15 16:37:20 +1100

    - separated history for retailer

--- src/purchaseHistory.js
+++ src/purchaseHistory.js
@@ -9,7 +9,20 @@ import axios from 'axios';
 export default class Shop extends Component {
     constructor(props){
         super(props);
-        this.state = {mode:'customer', items: []};
+        this.state = {mode:'rtc', items: []};
+
+        this.handleToRcust = this.handleToRcust.bind(this);
+        this.handleToRetail = this.handleToRetail.bind(this);
+    }
+
+    handleToRcust() {
+        this.setState({ mode: 'rtc' });
+        this.componentDidMount()
+    }
+
+    handleToRetail() {
+        this.setState({ mode: 'rt' });
+        this.componentDidMount()
     }
 
     componentDidMount() {
@@ -29,7 +42,7 @@ export default class Shop extends Component {
         axios.get('http://localhost:4000/history')
             .then((res) => {
                 for (var p = 0; p <= res.data.length; p++) {
-                    if (this.state.userType == 'Customer') {
+                    if (this.state.mode === 'rtc') {
                         if (res.data[p].customerId == localStorage.getItem('currentUserId')) {
                             items2[m] = res.data[p];
                             this.setState({items: items2})
@@ -53,8 +66,42 @@ export default class Shop extends Component {
 
     }
 
+
+    /*renderTest() {
+        if (this.state.userType == 'Retailer') {
+            return(
+                {this.state.items.map(function(currentItem, i) {
+                        return(
+                            <tr>
+                                <td>{currentItem.customerId}</td>
+                                <td>{currentItem.name}</td>
+                                <td>{currentItem.price}</td>
+                                <td><button class="btn btn-link" onClick={function() {
+                                    localStorage.setItem('id', currentItem._id)
+                                    axios.delete('http://localhost:4000/history/' + localStorage.getItem('id'))
+                                        .then(res => {window.location.reload(); alert("Deleted")});
+                                }}>X</button></td>
+                            </tr>
+                        )
+                    })
+                }
+            )
+        }
+    }*/
+
+    renderNothing(){
+        if (this.state.userType === "Customer"){
+            return null;
+        }
+        else{
+            return(
+                <button type="button" className="btn btn-primary" onClick={this.handleToRetail}>View Sales History</button>
+            );
+        }
+    }
+
     render() {
-        if (this.state.userType == 'Customer') {
+        if (this.state.mode === 'rtc') {
             return (
                 <div class="container">
                     <Router>
@@ -89,14 +136,64 @@ export default class Shop extends Component {
                                 }
                                 </tbody>
                                 </table>
+                                {this.renderNothing()}
                             </div>
                         </div>
-
                     </Router>
                 </div>
             );
         }
-        else{
+
+        if (this.state.mode === 'rt') {
+            return (
+                <div class="container">
+                    <Router>
+                        <div className="App">
+                            <div class="jumbotron">
+                                <h1 class="headingTop">Purchase History</h1>
+                                <a href="/custProfile">Return to Profile</a>
+                                <table className="table table-striped" style={{marginTop: 20}}>
+                                    <thead>
+                                    <tr>
+                                        <th>Buyer ID</th>
+                                        <th>Name</th>
+                                        <th>Price</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.items.map(function (currentItem, i) {
+                                        return (
+                                            <tr>
+                                                <td>{currentItem.customerId}</td>
+                                                <td>{currentItem.name}</td>
+                                                <td>{currentItem.price}</td>
+                                                <td>
+                                                    <button class="btn btn-link" onClick={function () {
+                                                        localStorage.setItem('id', currentItem._id)
+                                                        axios.delete('http://localhost:4000/history/' + localStorage.getItem('id'))
+                                                            .then(res => {
+                                                                window.location.reload();
+                                                                alert("Deleted")
+                                                            });
+                                                    }}>X
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                    }
+                                    </tbody>
+                                </table>
+                                <button type="button" className="btn btn-primary" onClick={this.handleToRcust}>View Purchase History</button>
+                            </div>
+                        </div>
+                    </Router>
+                </div>
+            );
+        }
+
+         if (this.state.userType === 'Retailer' && this.state.mode === 'rtc'){
             return (
                 <div class="container">
                     <Router>
@@ -107,7 +204,7 @@ export default class Shop extends Component {
                                 <table className="table table-striped" style={{ marginTop: 20 }} >
                                     <thead>
                                     <tr>
-                                        <th>Buyer ID</th>
+                                        <th>Retailer ID</th>
                                         <th>Name</th>
                                         <th>Price</th>
                                         <th>Delete</th>
@@ -117,7 +214,7 @@ export default class Shop extends Component {
                                     {this.state.items.map(function(currentItem, i) {
                                         return(
                                             <tr>
-                                                <td>{currentItem.customerId}</td>
+                                                <td>{currentItem.retailerId}</td>
                                                 <td>{currentItem.name}</td>
                                                 <td>{currentItem.price}</td>
                                                 <td><button class="btn btn-link" onClick={function() {
@@ -131,9 +228,9 @@ export default class Shop extends Component {
                                     }
                                     </tbody>
                                 </table>
+                                <button type="button" className="btn btn-primary" onClick={this.handleToRetail}>View Sales History</button>
                             </div>
                         </div>
-
                     </Router>
                 </div>
             );
