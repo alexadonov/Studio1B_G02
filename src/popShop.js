f89e124855d3565228577d1d170cd98c4f380bb0
alexadonov <alexadonovan4427@gmail.com>
2019-10-16 13:52:37 +1100

    testing

--- src/popShop.js
+++ src/popShop.js
@@ -14,13 +14,38 @@ export default class PopShop extends Component {
   constructor(props) {
        super(props);
        this.state = { 
-         items: []
+         items: [],
+         history: [],
+         countArr: []
        }
 
        this.addToCart = this.addToCart.bind(this);
     }
 
     componentDidMount() {
+      axios.get('http://localhost:4000/history/').then(res => {
+        this.setState({
+          history: res.data.productId
+        })
+        var hist = this.state.history
+
+        const uniqueId = hist.filter(function(item, index){
+          return res.data.productId.indexOf(item) === index
+        })
+
+        for(var i=0; i<uniqueId.length; i++){
+          var arr = []
+
+          const count = res.data.productId.filter(id => id == uniqueId[i])
+
+          arr[i] = {id: uniqueId[i], count: count.length}
+
+          this.setState({
+            countArr: arr
+          })
+        }
+      })
+
       axios.get('http://localhost:4000/items/')
           .then(res => {
             this.setState({
@@ -50,49 +75,22 @@ export default class PopShop extends Component {
                 <section class="pt-0 pb-5">
                             <div class="row pb-5">
                             <div class="card-deck py-3 px-4">
-                        {this.state.items.map(function(currentItem, i) {
-                          if (currentItem.popItem) {
+                        {this.state.countArr.map(function(currentItem, i) {
                             return (
                             <div class="row">
                             <div class="card-deck py-3 px-4">
                               <div class="card" key={i}>
-                                <img src={currentItem.image} class="card-img-top card-header" alt="placeholder"/>
                                 <div class="card-body">
-                                  <h5 class="card-title"><b>{currentItem.name}</b></h5>
-                                  <p class="card-text">{currentItem.description}</p>
+                                  <h5 class="card-title"><b>{currentItem.id}</b></h5>
+                                  <p class="card-text">{currentItem.count}</p>
                                 </div>
                                 <div class="card-footer">
-                                  <a class="price my-2"> <b><CurrencyFormat value={currentItem.price} displayType="text" thousandSeparator={true} prefix="$" /></b></a>
-                                  <button class="list-group-item list-group-item-action" onClick={function() {
-                                    // localStorage.setItem('productName', currentItem.name);
-                                    // localStorage.setItem('productId', currentItem._id);
-                                    // localStorage.setItem('productRetailerid', currentItem.retailerId);
-                                    // localStorage.setItem('productPrice', currentItem.price);
-
-                                    const newItem = {
-                                      customerId: localStorage.getItem('currentUserId'),
-                                      retailerId: currentItem.retailerId,
-                                      productId: currentItem._id,
-                                      name: currentItem.name,
-                                      price: currentItem.price
-                                    }
-                                    axios.post('http://localhost:4000/cart/add', newItem)
-                                      .then(response => {
-                                          alert("Item added to cart!")
-                                          return;
-                                      })
-                                      .catch(function (error){
-                                        console.log('What happened? ' + error);
-                                      })
-
-                                  }}>Add to Cart</button>
                                 </div>
                             </div>
                           </div>
                         </div>
 
                           )
-                      }
                         })
                       }
                          </div>
